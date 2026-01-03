0d87ccb9789ee1e77e75fabdde745ea6b400a575
Ahmed527 <naveedahmed151106@gmail.com>
2026-01-03 15:30:24 +0530

    Flowforge minor refactors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://flowforge-ai-drab.vercel.app/"],
+    origin: ["http://localhost:3000", "https://flowforge-ai-drab.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
